38e72528d70e82fc0614874e14ff2c58e231a31d
Yashwanth Reddy <yashknr2@gmail.com>
2025-01-16 01:54:06 +0530

    Update Register.jsx

--- Frontend/src/pages/Auth/Register.jsx
+++ Frontend/src/pages/Auth/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
   }
   async function handleSubmit(event){
     event.preventDefault()
-    const response = await fetch('http://127.0.0.1:3000/api/v1/users/signup',{
+    const response = await fetch('https://e-com-backend-4zqj.onrender.com/api/v1/users/signup',{
       method: 'POST',
       body: JSON.stringify(form),
       headers: {
