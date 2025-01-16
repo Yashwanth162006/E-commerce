3a7bfcc5556899bec249bd4ce7ba0c72c0e8fab1
Yashwanth Reddy <yashknr2@gmail.com>
2025-01-16 08:20:35 +0530

    Update Review.jsx

--- Frontend/src/pages/User/Review.jsx
+++ Frontend/src/pages/User/Review.jsx
@@ -8,7 +8,7 @@ const Review = (props)=>{
   const {_id} = useParams()
   const {loginToken} = useContext(ShopContext)
   async function deleteReview(){
-    const response = await fetch(`http://127.0.0.1:3000/api/v1/reviews/${_id}`,{
+    const response = await fetch(`https://e-com-backend-4zqj.onrender.com/api/v1/reviews/${_id}`,{
       method: 'DELETE',
       headers: {
          'Authorization': `Bearer ${loginToken}`
